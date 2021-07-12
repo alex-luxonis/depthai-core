1898f3b56c3490dcc2e9de90103e39dd3d8887c0
TheMarpe <martin.peterlin7@gmail.com>
2021-07-12 23:19:17 +0200

    Updated flash_bootloader example

--- examples/src/flash_bootloader.cpp
+++ examples/src/flash_bootloader.cpp
@@ -4,37 +4,54 @@
 #include "depthai/depthai.hpp"
 
 int main(int argc, char** argv) {
+    using namespace std;
     using namespace std::chrono;
 
     dai::DeviceBootloader::Type blType = dai::DeviceBootloader::Type::USB;
     if(argc > 1) {
-        if(std::string(argv[1]) == "usb") {
+        if(string(argv[1]) == "usb") {
             blType = dai::DeviceBootloader::Type::USB;
-        } else if(std::string(argv[1]) == "eth") {
+        } else if(string(argv[1]) == "eth") {
             blType = dai::DeviceBootloader::Type::NETWORK;
         } else {
-            std::cout << "Specify either 'usb' or 'eth' bootloader type\n";
+            cout << "Specify either 'usb' or 'eth' bootloader type\n";
             return 0;
         }
     } else {
-        std::cout << "Usage: " << argv[0] << " <usb/eth>\n";
+        cout << "Usage: " << argv[0] << " <usb/eth>\n";
+        return 0;
+    }
+
+    cout << "Flashing bootloader is a risky operation and can result in soft-bricking your device\n";
+    cout << "Type 'yes' and press enter if you understand the risk and want to continue\n";
+    string confirmation;
+    cin >> confirmation;
+    if(confirmation != "yes"){
+        cout << "Not flashing the bootloader" << endl;
         return 0;
     }
 
     bool res = false;
     dai::DeviceInfo info;
-    std::tie(res, info) = dai::DeviceBootloader::getFirstAvailableDevice();
+    tie(res, info) = dai::DeviceBootloader::getFirstAvailableDevice();
 
-    dai::DeviceBootloader bl(info);
-    auto progress = [](float p) { std::cout << "Flashing Progress..." << p * 100 << "%" << std::endl; };
-
-    std::string message;
-    auto t1 = steady_clock::now();
-    std::tie(res, message) = bl.flashBootloader(dai::DeviceBootloader::Memory::FLASH, blType, progress);
     if(res) {
-        std::cout << "Flashing successful. Took " << duration_cast<milliseconds>(steady_clock::now() - t1).count() << "ms" << std::endl;
+
+        dai::DeviceBootloader bl(info);
+        auto progress = [](float p) { cout << "Flashing Progress..." << p * 100 << "%" << endl; };
+
+        string message;
+        auto t1 = steady_clock::now();
+        tie(res, message) = bl.flashBootloader(dai::DeviceBootloader::Memory::FLASH, blType, progress);
+        if(res) {
+            cout << "Flashing successful. Took " << duration_cast<milliseconds>(steady_clock::now() - t1).count() << "ms" << endl;
+        } else {
+            cout << "Flashing failed: " << message << endl;
+        }
+
     } else {
-        std::cout << "Flashing failed: " << message << std::endl;
+        std::cout << "No devices found" << std::endl;
     }
+
     return 0;
 }
